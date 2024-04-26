535acd06f6be87472e1695d7b25d02aa305d96bf
Cássio Filipe Meireles De Carvalho Braga <78364193+Cassio06@users.noreply.github.com>
2024-04-26 16:16:09 -0300

    a
    a

--- teste/App.tsx
+++ teste/App.tsx
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>testando</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return(
+    <View><Text>
+      HELLO FDS</Text></View>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
