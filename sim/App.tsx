1a8a9effd620846f0238d71025032c9cfbdb3780
artur1612 <78364193+Cassio06@users.noreply.github.com>
2024-05-15 10:29:45 -0300

    asdawdad
    dasdadasdad

--- sim/App.tsx
+++ sim/App.tsx
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import { View, Text } from 'react-native';
+import { SingnIn } from './src/screens/index';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>a</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return(
+        <SingnIn />
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ sim/src/screens/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+export  function SingnIn() {
+    return(
+        <View>
+            <Text>Hello</Text>
+        </View>
+    );
+}   
