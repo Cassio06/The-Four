6a6cdae81f4dd43ea8158f88a9a36b19b8ef098a
Cássio Filipe Meireles De Carvalho Braga <78364193+Cassio06@users.noreply.github.com>
2024-05-19 11:29:54 -0300

    ASDAWD
    ASDAWDASDAWD

--- sim/src/screens/index.tsx
+++ sim/src/screens/index.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export  function SingnIn() {
     return(
         <View>
-            <Text>Hello</Text>
+            <Text>Hellos posrra</Text>
         </View>
     );
 }   
