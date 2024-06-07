7d1de1a376d408e94586787edd7db4c7ab39e930
StefanieLaura <160498121+porfirioJ@users.noreply.github.com>
2024-06-07 10:16:46 -0300

    knnn
    mmm

--- teste/src/screens/index.tsx
+++ teste/src/screens/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState }from 'react'  
 import { View, Text, TextInput } from 'react-native'
 import { styles } from './styles';
+import { StatusBar } from 'expo-status-bar'
 export function SingnIn() {
   const[text, setText] = useState ("Nada foi Digitado no momento");
   return(
     <View style={styles.container}>
       <Text>Sim</Text>
+      <StatusBar style='auto'/>
       <TextInput style ={styles.input}
       onChangeText={setText}/>
       
