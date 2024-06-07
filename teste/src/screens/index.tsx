675e0f5e9bc5f6ae0c33759060c507b72e7bb0f3
StefanieLaura <160498121+porfirioJ@users.noreply.github.com>
2024-06-07 10:34:03 -0300

    acasdasdadad
    asdsadasd

--- teste/src/screens/index.tsx
+++ teste/src/screens/index.tsx
@@ -1,19 +1,12 @@
 import React, { useState }from 'react'  
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, Image } from 'react-native'
 import { styles } from './styles';
 import { StatusBar } from 'expo-status-bar'
 export function SingnIn() {
   const[text, setText] = useState ("Nada foi Digitado no momento");
   return(
-    <View style={styles.container}>
-      <Text>Sim</Text>
-      <StatusBar style='auto'/>
-      <TextInput style ={styles.input}
-      onChangeText={setText}/>
-      
-      <Text>
-        Você digitou: {text}
-      </Text>
+    <View>
+      <
     </View>
   );
 } 
